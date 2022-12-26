6a633d8e01eac51537b8abf6baf0bcfc5e60da16
Zoran Dori <zorandori4444@gmail.com>
2022-12-26 06:59:55 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+/* eslint-env node, es6 */
+module.exports = function ( grunt ) {
+
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				cache: true,
+				fix: grunt.option( 'fix' )
+			},
+			all: '.'
+		},
+		stylelint: {
+			all: [
+				'**/*.{css,less}',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
