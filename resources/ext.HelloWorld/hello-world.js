5e50b1485d7c407042dc23d616bd02683522fa2d
Patrick Schleizer <adrelanos@whonix.org>
2023-01-31 10:47:37 -0500

    Fix JavaScript syntax error (#6)

--- resources/ext.HelloWorld/hello-world.js
+++ resources/ext.HelloWorld/hello-world.js
@@ -1,5 +1,5 @@
 mw.loader.using( [ 'mediawiki.util' ], function () {
-	$.ready( document ).ready( function () {
+	$( document ).ready( function () {
 		// eslint-disable-next-line no-jquery/no-global-selector
 		$( 'body' ).append(
 			$( '<div>' ).attr( 'id', 'hello-world' )
